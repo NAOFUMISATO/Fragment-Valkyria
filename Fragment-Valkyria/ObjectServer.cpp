5d47eab30eba577f81a33f5be8d7288c12fcbed4
SatoNaofumi <121pg0011@tokyo.amg.ac.jp>
2022-01-01 14:19:45 +0900

    ObjectServerコメント追記

--- Fragment-Valkyria/ObjectServer.cpp
+++ Fragment-Valkyria/ObjectServer.cpp
@@ -12,19 +12,20 @@ using namespace FragmentValkyria::Object;
 
 void ObjectServer::Add(std::unique_ptr<ObjectBase> obj) {
    if (_updating) {
-      // 更新中は_pendingActorsに追加する
+      // 更新中は保留中の動的配列に追加する
       _pendingObjects.emplace_back(std::move(obj));
    }
    else {
+      //更新中でなければ処理を回す用の動的配列に追加する
       _runObjects.emplace_back(std::move(obj));
    }
 }
-/// 入力
+
 void ObjectServer::Input(AppFrame::Input::InputManager& input) {
    _updating = true;
+   // _runObjectsに登録されているActive状態のオブジェクトの入力処理を回す
    for (auto&& obj : _runObjects) {
       if (obj->IsActive()) {
-         // アクターに入力する
          obj->Input(input);
       }
    }
@@ -33,59 +34,59 @@ void ObjectServer::Input(AppFrame::Input::InputManager& input) {
 /// 更新
 void ObjectServer::Update() {
    _updating = true;
+   // _runObjectsに登録されているActive状態のオブジェクトの更新処理を回す
    for (auto&& obj : _runObjects) {
       if (obj->IsActive()) {
-         // アクターに入力する
          obj->Update();
       }
    }
    _updating = false;
 
-   // 保留中のアクターを_actorsに移動する
+   // 保留中のオブジェクトを_runObjectsに移動する
    _runObjects.insert(_runObjects.end(),
       make_move_iterator(_pendingObjects.begin()),
       make_move_iterator(_pendingObjects.end()));
 
-   // 保留中のアクターをクリアする
+   // 保留中のオブジェクトを削除する
    _pendingObjects.clear();
 
    // 死んだアクターを削除する
    erase_if(_runObjects, [](auto&& act) { return act->IsDead(); });
 }
-/// 描画
+
 void ObjectServer::Render() {
    for (auto&& obj : _runObjects) {
+      // _runObjectsに登録されているActive状態のオブジェクトの描画処理を回す
       if (obj->IsActive()) {
          obj->Draw();
       }
    }
 }
-/// 全アクターの削除
+
 void ObjectServer::Clear() {
+   // 全ての動的配列内の要素を削除する
    _runObjects.clear();
    _pendingObjects.clear();
 }
-/// アクターの登録
+
 void ObjectServer::Register(std::string_view key, Vector4 vec) {
    if (_objRegistry.contains(key.data())) {
       _objRegistry[key.data()] = vec;
    }
    _objRegistry.emplace(key, vec);
 }
-/// アクターの一覧の取得
+
 AppFrame::Math::Vector4 ObjectServer::GetPosition(std::string_view key) {
    if (_objRegistry.contains(key.data())) {
       return _objRegistry[key.data()];
    }
-   // 未登録
    return { 0, 0, 0 };
 }
-/// 登録したアクターの位置を得る
+
 AppFrame::Math::Vector4 ObjectServer::GetForward(std::string_view key) {
    if (_objRegistry.contains(key.data())) {
       return _objRegistry[key.data()];
    }
-   // 未登録
    return { 0, 0, 0 };
 }
 
