b683435403e3838a0a52a5d275b6dc8f1c798880
SatoNaofumi <121pg0011@tokyo.amg.ac.jp>
2022-01-15 00:43:00 +0900

    Loadjsonコメント追記

--- AppFrame/LoadJson.cpp
+++ AppFrame/LoadJson.cpp
@@ -140,10 +140,10 @@ namespace AppFrame {
          auto soundDirectory = _gameBase.pathServer().GetCurrentPath("Sound") / jsonName;
          // トップレベルの配列を全て回し、格納している音源情報を全て取り出す
          for (auto& soundData : soundArray) {
-            const auto keyName = soundData["keyname"];   //!< キー
-            const auto fileName = soundData["filename"]; //!< ファイル名
-            const auto isLoad = soundData["isload"];     //!< 事前読み込みの有無
-            const auto volume = soundData["volume"];     //!< 初期音量
+            const auto keyName = soundData["keyname"];   // キー
+            const auto fileName = soundData["filename"]; // ファイル名
+            const auto isLoad = soundData["isload"];     // 事前読み込みの有無
+            const auto volume = soundData["volume"];     // 初期音量
             // ファイルへのパスを形成
             const auto soundPath = (soundDirectory / fileName).generic_string();
             // 取り出したキー及び音源へのファイルパスと事前読み込みの有無と初期音量のtuple型をResourceServerに登録する
@@ -179,9 +179,9 @@ namespace AppFrame {
          auto effectDirectory = _gameBase.pathServer().GetCurrentPath("Effect") / jsonName;
          // トップレベルの配列を全て回し、格納しているエフェクト情報を全て取り出す
          for (auto& effectData : effectArray) {
-            const auto keyName = effectData["keyname"];    //!< キー
-            const auto fileName = effectData["filename"];  //!< ファイル名
-            const auto scale = effectData["scale"];        //!< 初期拡大率
+            const auto keyName = effectData["keyname"];    // キー
+            const auto fileName = effectData["filename"];  // ファイル名
+            const auto scale = effectData["scale"];        // 初期拡大率
             // ファイルへのパスを形成
             const auto effectPath = (effectDirectory / fileName).generic_string();
             // 取り出したキー及びエフェクトへのファイルパスと初期拡大率のpair型をResourceServerに登録する
@@ -273,21 +273,25 @@ namespace AppFrame {
          std::unordered_map<std::string, nlohmann::json> paramMap;
          // 引数に指定した文字列の動的配列を全て回す
          for (auto i = 0; i < paramNames.size(); i++) {
-            // トップレベルの配列を全て回し、格納している引数の文字列に一致する各データを全て取り出す
+            // トップレベルの配列を全て回し、引数の文字列に一致する格納している各データを全て取り出す
             for (auto& paramData : paramArray) {
                const auto param = paramData[paramNames[i].data()];
                // 各データを取り出した際の文字列をキー、取り出したnlohmann::json型データを連想配列に登録する
                paramMap.emplace(paramNames[i], param);
             }
          }
+         // 各データを登録した連想配列を返す
          return paramMap;
       }
 
       std::unordered_map<std::string, Math::Vector4> LoadJson::GetVecParamMap(const std::filesystem::path jsonName, const std::vector<std::string_view> vecParamNames) {
+         // 各Vector4データを格納しているjsonフォルダへのパスを、ゲーム本体側で実体を定義した静的メンバから取得する
          auto jsonDirectory = _paramJsonCurrentPath;
          auto jsonPath = (jsonDirectory / jsonName).generic_string() + ".json";
+         // 指定したjsonファイルを読み取り専用で開く
          std::ifstream reading(jsonPath, std::ios::in);
 #ifdef _DEBUG
+         // 指定したファイルを開くのに失敗したならlogic_errorを検出し、デバッガに出力する
          try {
             if (reading.fail()) {
                throw std::logic_error("----------" + jsonPath + "ファイルが開けませんでした。----------\n");
@@ -298,22 +302,32 @@ namespace AppFrame {
          }
 #endif
          nlohmann::json value;
+         // ファイルの中身を取り出す
          reading >> value;
+         // ファイルを閉じる
          reading.close();
+         // トップレベルの配列のキーを、引数から指定し取り出す
          auto vecArray = value[jsonName.generic_string()+"_vec"];
+         // 各Vector4データを格納する連想配列を定義する
          std::unordered_map<std::string, Vector4> vecParamMap;
+         // 引数に指定した文字列の動的配列を全て回す
          for (auto i = 0; i < vecParamNames.size(); i++) {
+            // トップレベルの配列を全て回し、引数の文字列に一致する格納している各Vector4データを全て取り出す
             for (auto& vecData : vecArray) {
                auto vecParamArray = vecData[vecParamNames[i].data()];
+               // 取り出したVector4データの各要素を取り出す
                for (auto& vecParam : vecParamArray) {
-                  const auto x = vecParam["x"];
-                  const auto y = vecParam["y"];
-                  const auto z = vecParam["z"];
+                  const auto x = vecParam["x"];   // x成分
+                  const auto y = vecParam["y"];   // y成分
+                  const auto z = vecParam["z"];   // z成分
+                  // Vector4クラスを生成し、Vector4データを登録
                   Vector4 vec = Vector4(x, y, z);
+                  // Vector4データを取り出した際の文字列をキー、取り出したVector4データを連想配列に登録する
                   vecParamMap.emplace(vecParamNames[i],vec);
                }
             }
          }
+         // 各Vector4データを登録した連想配列を返す
          return vecParamMap;
       }
    }
