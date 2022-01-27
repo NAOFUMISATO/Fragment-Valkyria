91588ac4c5956741b8fbeb5c217b9a0c091e3518
SatoNaofumi <121pg0011@tokyo.amg.ac.jp>
2022-01-27 23:03:27 +0900

    SpriteBaseRoot修正

--- AppFrame/SpriteBaseRoot.cpp
+++ AppFrame/SpriteBaseRoot.cpp
@@ -12,13 +12,10 @@ namespace AppFrame {
       SpriteBaseRoot::SpriteBaseRoot(Game::GameBase& gameBase) :_gameBase{ gameBase } {
       }
 
-      SpriteBaseRoot::~SpriteBaseRoot() {
-      }
-
       void SpriteBaseRoot::Draw() {
          auto [x, y] = _position.GetVec2();
          SetDrawBlendMode(DX_BLENDMODE_ALPHA, _alpha);
-         GetSimpTexComponent().TransDrawTexture(x, y, _cx, _cy, _scale, _angle, _grHandle, _turnFlag);
+         GetSimpTexComponent().TransDrawTexture(static_cast<int>(x), static_cast<int>(y), _cx, _cy, _scale, _angle, _grHandle, _turnFlag);
          SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
       }
 
