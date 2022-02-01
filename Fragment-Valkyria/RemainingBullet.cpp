1af036902736f75effba7b4e4aa357a42720c95e
NAOFUMISATO <87857828+NAOFUMISATO@users.noreply.github.com>
2022-02-01 13:22:47 +0900

    ModeClearResultクラス仮作成

--- Fragment-Valkyria/RemainingBullet.cpp
+++ Fragment-Valkyria/RemainingBullet.cpp
@@ -3,9 +3,14 @@
 
 namespace{
    auto paramMap = AppFrame::Resource::LoadParamJson::GetParamMap("playerui", { "bullet_diff_x" });
-   const double BulletDiffX = paramMap["bullet_diff_x"];
+   const double DiffX = paramMap["bullet_diff_x"];
    auto vecParamMap = AppFrame::Resource::LoadParamJson::GetVecParamMap("playerui", { "bullet_pos" });
    const auto BulletPos = vecParamMap["bullet_pos"];
+   constexpr auto ThirdMultRate = 2.0;
+   constexpr auto FourthMultRate = 3.0;
+   constexpr auto FifthMultRate = 4.0;
+   constexpr auto DefalutScale = 1.0;
+   constexpr auto DefalutAngle = 0.0;
 }
 
 using namespace FragmentValkyria::Player;
@@ -19,10 +24,10 @@ void RemainingBullet::Init() {
    auto [x,y] = _position.GetVec2();
    _bulletPosX = { 
       static_cast<int>(x),
-      static_cast<int>(x + BulletDiffX * 1.0),
-      static_cast<int>(x + BulletDiffX * 2.0),
-      static_cast<int>(x + BulletDiffX * 3.0),
-      static_cast<int>(x + BulletDiffX * 4.0)
+      static_cast<int>(x + DiffX),
+      static_cast<int>(x + DiffX * ThirdMultRate),
+      static_cast<int>(x + DiffX * FourthMultRate),
+      static_cast<int>(x + DiffX * FifthMultRate)
    };
 }
 
@@ -35,11 +40,11 @@ void RemainingBullet::Draw() {
    auto [x, y] = _position.GetVec2();
    auto [firstFlag, secondFlag, thirdFlag, fourthFlag, fifthFlag] = _stockFlag;
    auto [firstX, secondX, thirdX, fourthX, fifthX] = _bulletPosX;
-   GetSimpTexComponent().DrawTexture(firstX, static_cast<int>(y), 1.0, 0.0, _grHandles[firstFlag]);
-   GetSimpTexComponent().DrawTexture(secondX, static_cast<int>(y), 1.0, 0.0, _grHandles[secondFlag]);
-   GetSimpTexComponent().DrawTexture(thirdX, static_cast<int>(y), 1.0, 0.0, _grHandles[thirdFlag]);
-   GetSimpTexComponent().DrawTexture(fourthX, static_cast<int>(y), 1.0, 0.0, _grHandles[fourthFlag]);
-   GetSimpTexComponent().DrawTexture(fifthX, static_cast<int>(y), 1.0, 0.0, _grHandles[fifthFlag]);
+   GetSimpTexComponent().DrawTexture(firstX, static_cast<int>(y), DefalutScale, DefalutAngle, _grHandles[firstFlag]);
+   GetSimpTexComponent().DrawTexture(secondX, static_cast<int>(y), DefalutScale, DefalutAngle, _grHandles[secondFlag]);
+   GetSimpTexComponent().DrawTexture(thirdX, static_cast<int>(y), DefalutScale, DefalutAngle, _grHandles[thirdFlag]);
+   GetSimpTexComponent().DrawTexture(fourthX, static_cast<int>(y), DefalutScale, DefalutAngle, _grHandles[fourthFlag]);
+   GetSimpTexComponent().DrawTexture(fifthX, static_cast<int>(y), DefalutScale, DefalutAngle, _grHandles[fifthFlag]);
 }
 
 void RemainingBullet::StockCheck(int stock) {
