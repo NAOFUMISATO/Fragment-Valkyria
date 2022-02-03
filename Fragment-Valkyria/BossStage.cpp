02f1117037d6eec55efca981b5628da399056980
SatoNaofumi <121pg0011@tokyo.amg.ac.jp>
2022-02-04 03:28:50 +0900

    Stageコリジョン透明化描画

--- Fragment-Valkyria/BossStage.cpp
+++ Fragment-Valkyria/BossStage.cpp
@@ -21,6 +21,22 @@ BossStage::BossStage(Game::GameMain& game) : Object::ObjectBase{ game } {
    _stage = std::make_unique<StageModelComponent>(*this);
    _stage->SetModels("Boss");
    _stage->PixelLightingON();
+
+#ifndef _DEBUG
+   auto [charaCollHandle, charaCollNum] = _stage->GetHandleAndCollNum("stage_character_c");
+   auto [bossCollHandle, bossCollNum] = _stage->GetHandleAndCollNum("stage_boss_c");
+   auto [objCollHandle, objCollNum] = _stage->GetHandleAndCollNum("stage_object_c");
+   MV1SetFrameVisible(charaCollHandle, charaCollNum, false);
+   MV1SetFrameVisible(bossCollHandle, bossCollNum, false);
+   MV1SetFrameVisible(objCollHandle, objCollNum, false);
+#else
+   auto [charaCollHandle,charaCollNum] = _stage->GetHandleAndCollNum("stage_character_c");
+   auto [bossCollHandle,bossCollNum] = _stage->GetHandleAndCollNum("stage_boss_c");
+   auto [objCollHandle,objCollNum] = _stage->GetHandleAndCollNum("stage_object_c");
+   MV1SetFrameOpacityRate(charaCollHandle, charaCollNum,0.6f);
+   MV1SetFrameOpacityRate(bossCollHandle, bossCollNum, 1.0f);
+   MV1SetFrameOpacityRate(objCollHandle, objCollNum, 0.2f);
+#endif
 }
 
 void BossStage::Update() {
