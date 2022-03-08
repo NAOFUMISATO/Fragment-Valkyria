3302b53b8acd8910c80dceaaffced445baf9460b
SatoNaofumi <121pg0011@tokyo.amg.ac.jp>
2022-03-08 14:39:33 +0900

    PoorEnemy新クラス作成

---
+++ Fragment-Valkyria/PoorEnemyAlmighty.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "PoorEnemyBase.h"
+
+namespace FragmentValkyria {
+   namespace Enemy {
+      class PoorEnemyAlmighty :public PoorEnemyBase {
+         using Vector4 = AppFrame::Math::Vector4;
+      public:
+         PoorEnemyAlmighty(Game::GameMain& gameMain);
+         /**
+          * \brief 初期化処理
+          */
+         void Init()override;
+
+      private:
+         /**
+          * \brief 突進攻撃の処理
+          * \param moved 突進する方向の正規化ベクトル
+          */
+         void Rush(const Vector4& moved);
+         /**
+          * \brief ガトリング弾の生成処理
+          */
+         void CreateGatling();
+
+         Vector4 _gatlingMoveDirection{ Vector4(0.0, 0.0, 0.0) }; //!< ガトリング弾を放つ正規化ベクトル
+
+      public:
+         /**
+          * \class 突進攻撃状態クラス
+          * \brief 突進攻撃状態の処理を行う
+          */
+         class StateRush : public StateBase {
+         public:
+            /**
+             * \brief コンストラクタ
+             * \param owner 近接攻撃タイプの雑魚敵クラスの参照
+             */
+            StateRush(PoorEnemyAlmighty& owner) : StateBase{ owner }, _owner{ owner } {};
+            /**
+             * \brief 入口処理
+             */
+            void Enter() override;
+            /**
+             * \brief 更新処理
+             */
+            void Update() override;
+
+         private:
+            PoorEnemyAlmighty& _owner;    //!< 近接攻撃タイプの雑魚敵クラスの参照
+            Vector4 _moved{ 0,0,0 };   //!< 突進する方向の正規化ベクトル
+         };
+         /**
+         * \class 移動状態クラス
+         * \brief 移動状態の処理を回す
+         */
+         class StateGatling : public StateBase {
+         public:
+            /**
+             * \brief コンストラクタ
+             * \param owner ガトリング攻撃をしてくる雑魚敵の参照
+             */
+            StateGatling(PoorEnemyAlmighty& owner) : StateBase{ owner }, _owner{ owner } {};
+            /**
+             * \brief 入口処理
+             */
+            void Enter() override;
+            /**
+             * \brief 更新処理
+             */
+            void Update() override;
+
+         private:
+            PoorEnemyAlmighty& _owner;     //!< 遠隔攻撃タイプの雑魚敵クラスの参照
+            int _remainingGatiling{ 5 };  //!< ガトリングの残弾数
+         };
+   }
+}
--- Fragment-Valkyria/PoorEnemyGatling.h
+++ Fragment-Valkyria/PoorEnemyGatling.h
@@ -20,53 +20,52 @@ namespace FragmentValkyria {
        * \brief 遠隔攻撃タイプの雑魚敵の処理を回す
        */
       class PoorEnemyGatling : public PoorEnemyBase {
-            using Vector4 = AppFrame::Math::Vector4;
-            using InputManager = AppFrame::Input::InputManager;
-        public:
-           /**
-            * \brief コンストラクタ
-            * \param gameMain ゲーム本体クラスの参照
-            */
-            PoorEnemyGatling(Game::GameMain& gameMain);
-            /**
-             * \brief 初期化処理
-             */
-            void Init()override;
+         using Vector4 = AppFrame::Math::Vector4;
+         using InputManager = AppFrame::Input::InputManager;
+      public:
+         /**
+          * \brief コンストラクタ
+          * \param gameMain ゲーム本体クラスの参照
+          */
+         PoorEnemyGatling(Game::GameMain& gameMain);
+         /**
+          * \brief 初期化処理
+          */
+         void Init()override;
 
-        private:
-           /**
-            * \brief ガトリング弾の生成処理
-            */
-            void CreateGatling();
+      private:
+         /**
+          * \brief ガトリング弾の生成処理
+          */
+         void CreateGatling();
 
-            Vector4 _gatlingMoveDirection{ Vector4(0.0, 0.0, 0.0) }; //!< ガトリング弾を放つ正規化ベクトル
+         Vector4 _gatlingMoveDirection{ Vector4(0.0, 0.0, 0.0) }; //!< ガトリング弾を放つ正規化ベクトル
 
       public:
+         /**
+         * \class 移動状態クラス
+         * \brief 移動状態の処理を回す
+         */
+         class StateGatling : public StateBase {
+         public:
             /**
-            * \class 移動状態クラス
-            * \brief 移動状態の処理を回す
-            */
-            class StateGatling : public StateBase{
-            public:
-                /**
-                 * \brief コンストラクタ
-                 * \param owner ガトリング攻撃をしてくる雑魚敵の参照
-                 */
-                StateGatling(PoorEnemyGatling& owner) : StateBase{ owner }, _owner{ owner } {};
-                /**
-                 * \brief 入口処理
-                 */
-                void Enter() override;
-                /**
-                 * \brief 更新処理
-                 */
-                void Update() override;
+             * \brief コンストラクタ
+             * \param owner ガトリング攻撃をしてくる雑魚敵の参照
+             */
+            StateGatling(PoorEnemyGatling& owner) : StateBase{ owner }, _owner{ owner } {};
+            /**
+             * \brief 入口処理
+             */
+            void Enter() override;
+            /**
+             * \brief 更新処理
+             */
+            void Update() override;
 
-            private:
-               PoorEnemyGatling& _owner;     //!< 遠隔攻撃タイプの雑魚敵クラスの参照
-               int _remainingGatiling{ 5 };  //!< ガトリングの残弾数
-            };
+         private:
+            PoorEnemyGatling& _owner;     //!< 遠隔攻撃タイプの雑魚敵クラスの参照
+            int _remainingGatiling{ 5 };  //!< ガトリングの残弾数
+         };
       };
    }
-
 }
